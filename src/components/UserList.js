bfc06f924c954722a3fce1a7312d72649ceb5520
Siddhi2611 <114943172+Siddhi2611@users.noreply.github.com>
2025-10-07 17:03:56 +0530

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,42 +1,36 @@
 import React, { useEffect, useState } from "react";
 
-const UserList = ({ onUserClick }) => {
+const UserList = ({ navigate }) => {
   const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/users");
-        const data = await res.json();
-        setUsers(data);
-      } catch (err) {
-        console.error("Failed to fetch users", err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchUsers();
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error("Error fetching users:", err));
   }, []);
 
   return (
     <div>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>
-              <a href={`/users/${user.id}`} onClick={(e) => onUserClick(user.id, e)}>
-                {user.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      )}
+      <h1>User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {/* use <a> tag for Cypress exact href check */}
+            <a
+              href={`/users/${user.id}`}
+              onClick={(e) => {
+                e.preventDefault();
+                navigate(`/users/${user.id}`);
+              }}
+            >
+              {user.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default UserList;
-

97f424d8be35b2c07450fd94dca461740c0e46ff
Siddhi2611 <114943172+Siddhi2611@users.noreply.github.com>
2025-10-07 16:53:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
+import "../styles/App.css";
 
 const App = () => {
+  const [selectedUserId, setSelectedUserId] = useState(null);
+
+  const handleUserClick = (id, e) => {
+    e.preventDefault(); // prevent full reload
+    setSelectedUserId(id);
+  };
+
+  const handleBack = () => {
+    setSelectedUserId(null);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>User List</h1>
+      {!selectedUserId ? (
+        <UserList onUserClick={handleUserClick} />
+      ) : (
+        <UserDetails userId={selectedUserId} onBack={handleBack} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
